d9dfff8a218e4fc1c2cd4a3470360e2d1b57de2d
LizardiGabriel <jlizardir1800@alumno.ipn.mx>
2024-01-14 21:00:36 -0600

    ya quedo

--- untitled/src/clases/parser/ExprUnary.java
+++ untitled/src/clases/parser/ExprUnary.java
@@ -21,9 +21,30 @@ public class ExprUnary extends Expression{
         Object derechita = right.resolver(tablita);
         switch (operator.getTipo()) {
             case BANG:
+                if (derechita instanceof Double) {
+                    throw new RuntimeException("No se puede negar un double");
+                }
+                if(derechita instanceof Integer){
+                    throw new RuntimeException("No se puede negar un int");
+                }
+                if (derechita instanceof String) {
+                    throw new RuntimeException("No se puede negar un string");
+                }
                 return !(boolean)derechita;
             case MINUS:
-                return -(double)derechita;
+                if (derechita instanceof Boolean) {
+                    throw new RuntimeException("No se puede restar un boolean");
+                }
+                if (derechita instanceof String) {
+                    throw new RuntimeException("No se puede restar un string");
+                }
+                if(derechita instanceof Integer){
+                    return -(int)derechita;
+                }
+                if (derechita instanceof Double) {
+                    return -(double)derechita;
+                }
+                throw new RuntimeException("No se puede restar un " + derechita.getClass().getSimpleName());
             default:
                 throw new RuntimeException("Operador desconocido: " + operator.getLexema());
         }
